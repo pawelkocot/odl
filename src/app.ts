5a6bffbab144f6b4562024e5e8c47e32b27a3975
Paweł Kocot <kocot.pawel@gmail.com>
2016-03-16 17:02:48 +0100

    x

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,49 @@
 import {Component} from 'angular2/core';
-
-import * as Firebase from 'firebase';
+import 'firebase';
 
 @Component({
     selector: 'app',
-    template: 'AppComponent'
+    template: `
+        <button *ngIf="!user" (click)="logIn()">Log in</button>
+        <div *ngIf="user">
+            {{ user }}
+            <button (click)="logOut()">Log out</button>
+        </div>
+        `
 })
 export class AppComponent {
-    messagesRef:Firebase;
+    public firebase:Firebase;
+    public user:Object = null;
 
-    constructor() {
-        console.log(Firebase);
-        this.messagesRef = new Firebase('https://angular-connect.firebaseio.com/messages');
-        console.log(this.messagesRef);
+    ngOnInit() {
+        //".write": "auth != null && auth.uid == 'd571635f-65c9-44b7-a3aa-bc2b761e70e9'",
+        this.firebase = new Firebase('https://odl.firebaseio.com/messages');
+        this.firebase.onAuth((authData:FirebaseAuthData) => {
+            console.log(authData);
+            this.user = authData;
+        });
+
+        //this.messagesRef.on('value', (value) => console.log('on value', value.val()), (error) => console.log('on value failed', error));
+        //this.messagesRef.on('child_added', (value) => console.log('on child_added', value.val()), (error) => console.log('on child_added failed', error));
+
+        //this.messagesRef.push({
+        //    date: new Date().getTime(),
+        //    title: "Hello World!",
+        //    author: "Firebase",
+        //}).then(
+        //    (data) => console.log('push success', data),
+        //    (error) => console.log('push error', error)
+        //);
+    }
+
+    logIn() {
+        this.firebase.authWithPassword({
+            email: 'kocot.pawel@gmail.com',
+            password: 'aaaaa'
+        });
+    }
+
+    logOut() {
+        this.firebase.unauth();
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-
-
+import 'angular2/bundles/angular2-polyfills';
 import 'reflect-metadata';
+
 import {bootstrap} from 'angular2/platform/browser';
+import {enableProdMode} from 'angular2/core';
 import {AppComponent} from './app';
 
+enableProdMode();
 bootstrap(AppComponent);
